93166b7358a3cee4f4b5da7d217af3410283f828
Kevin Schaffner <kschaffner@guidehousefederal.com>
2022-09-19 21:19:59 -0400

    style edits, added first page

--- app.py
+++ app.py
@@ -4,7 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("testing.html")
+    return render_template("index.html")
+
+@app.route('/toaster_ovens')
+def toaster_ovens():
+    return render_template("toaster_ovens.html")
 
 if __name__ == '__main__':
     app.run()
